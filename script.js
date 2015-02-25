bdefc8c09bd5391d1f5a03c9cf956e18d803dad9
mranstrand <michael.ranstrand@gmail.com>
2015-02-25 13:34:21 +0100

    Massa pacar

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-var x = 100,
-    y = 100,
-    gingerguyImg;
+var pacman,
+    pacmanDiv, updateStartTime, requestId, timeSinceLastUpdate;
 
 function start() {
-    pacmanDiv = document.getElementById("pacman");
-    animationStartTime = window.performance.now();
-    requestId = window.requestAnimationFrame(draw);
+
+    pacmen = [];
+
+    for (var i = 0; i < 300; i++) {
+        pacmen.push(new Pacman());
+    }
+
+    updateStartTime = window.performance.now();
+    requestId = window.requestAnimationFrame(update);
+
 }
 
-function draw(timeStamp) {
-    console.log((50 + (timeStamp - animationStartTime)) + "px");
-    pacmanDiv.style.left = (50 + (timeStamp - animationStartTime) / 10) + "px";
+function update(timeStamp) {
 
+    timeSinceLastUpdate = timeStamp - updateStartTime;
+    updateStartTime = window.performance.now();
 
+    //Updatera positioner
+    for (var i = 0; i < pacmen.length; i++) {
+        pacmen[i].move(timeSinceLastUpdate);
+    }
 
-    requestAnimationFrame(draw);
+    requestAnimationFrame(update);
+}
+
+function Pacman() {
+    this.x = Math.random() * window.innerWidth;
+    this.y = Math.random() * window.innerHeight;
+    this.speedX = Math.random() * 200 - 100; // Hastighet i px/s (slumpas mellan -100 och 100)
+    this.speedY = Math.random() * 200 - 100; //Hastighet i px/s (slumpas mellan -100 och 100)
+    this.pacmanDiv = document.createElement("div");
+    this.pacmanDiv.className = "pacmanDiv";
+    document.body.appendChild(this.pacmanDiv);
+
+    this.move = function (timeSinceLastMove) {
+        this.x += (timeSinceLastMove * this.speedX) / 1000;
+        this.y += (timeSinceLastMove * this.speedY) / 1000;
+        this.pacmanDiv.style.left = this.x + "px";
+        this.pacmanDiv.style.top = this.y + "px";
+    }
+
 }
